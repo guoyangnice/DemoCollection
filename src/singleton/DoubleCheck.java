8e63ff87d68c2b975f81255d7827b5873f8ef74a
guoyangnice <yangjienice@126.com>
2021-03-27 21:23:30 +0800

    单例模式-增加双检锁

---
+++ src/singleton/DoubleCheck.java
@@ -0,0 +1,16 @@
+package singleton;
+//双检锁模式-线程安全，这里注意下volatile和synchronized的作用
+public class DoubleCheck {
+    public static volatile DoubleCheck doubleCheck = null;
+
+    public static DoubleCheck getInstance(){
+        if(doubleCheck == null){
+            synchronized (DoubleCheck.class){
+                if(doubleCheck == null){
+                    return new DoubleCheck();
+                }
+            }
+        }
+        return doubleCheck;
+    }
+}
