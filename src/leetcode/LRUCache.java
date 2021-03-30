f9c35f39a2cfa46b0469a76d2d37afbd1e7d4063
guoyangnice <yangjienice@126.com>
2021-03-30 22:50:32 +0800

    leetcode-mid-lru

--- src/leetcode/LRUCache.java
+++ src/leetcode/LRUCache.java
@@ -2,6 +2,7 @@ package leetcode;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+//这里使用的是最通用的写法，LinkedHashMap的插入模式，也可以使用LinkedHashMap的访问模式，更加简单
 
 public class LRUCache {
     int capacity;
@@ -12,6 +13,11 @@ public class LRUCache {
         map = new LinkedHashMap<>();
     }
 
+    /**
+     * 访问，会把最经常访问的，放在最后一个
+     * @param key
+     * @return
+     */
     public int get(int key){
         if(!map.containsKey(key)){
             return -1;
@@ -20,5 +26,23 @@ public class LRUCache {
         Integer value = map.remove(key);
         map.put(key,value);
         return value;
+    }
+
+    /**
+     * 会把新放入的放在最后一个
+     * @param key
+     * @param value
+     */
+    public void put(int key,int value){
+        if(map.containsKey(key)){
+            map.remove(key);
+            map.put(key,value);
+            return;
+        }
+        map.put(key,value);
+        //超出capacity，删除最久没用的，利用迭代器删除第一个
+        if(map.size() > capacity){
+            map.remove(map.entrySet().iterator().next().getKey());
+        }
     }
 }
