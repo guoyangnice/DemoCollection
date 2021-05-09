845ed03cb7fa4a7ca59c5aec0cee7afa850e4e3a
guoyangnice <yangjienice@126.com>
2021-05-09 18:44:05 +0800

    j@feat:动态规划系列-最长上升子序列

---
+++ src/leetcode/dynamicprogramming/LongestAscendingSub.java
@@ -0,0 +1,32 @@
+package leetcode.dynamicprogramming;
+
+/**
+ * 题目：给定一个无序的整数数组，找到其中最长上升子序列的长度。
+ *
+ * @author yangjie
+ */
+public class LongestAscendingSub {
+    public static int longest(int[] nums) {
+        int length = nums.length;
+        if (length < 0) {
+            return 0;
+        }
+        int[] dp = new int[length];
+        dp[0] = nums[0];
+        int result = 1;
+        for (int i = 1; i < length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            result = Math.max(result, dp[i]);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 9, 5, 9, 3};
+        System.out.println(longest(nums));
+    }
+}
