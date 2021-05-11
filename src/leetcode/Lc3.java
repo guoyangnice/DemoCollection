d3b823e6505f6c3a30f338f2d86ef5c6dcd3c8aa
guoyangnice <yangjienice@126.com>
2021-05-12 00:22:14 +0800

    yj@feat:最长无重复字串的长度

--- src/leetcode/Lc3.java
+++ src/leetcode/Lc3.java
@@ -1,2 +1,33 @@
-package leetcode;public class Lc3 {
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 无重复字符的最长子串的长度
+ *
+ * @author yangjie
+ */
+public class Lc3 {
+    public static void main(String[] args) {
+        int[] arr = {2, 2, 3, 4, 5, 3};
+        System.out.println(maxLength(arr));
+    }
+
+    public static int maxLength(int[] arr) {
+        int res = 0;
+        int length = arr.length;
+        if(length == 0 || length == 1){
+            return length;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int left = 0, right = 0; right < length; right++) {
+            if(map.containsKey(arr[right])){
+                left = Math.max(left,map.get(arr[right]) + 1);
+            }
+            map.put(arr[right],right);
+            res = Math.max(res,right - left + 1);
+        }
+        return res;
+    }
 }
