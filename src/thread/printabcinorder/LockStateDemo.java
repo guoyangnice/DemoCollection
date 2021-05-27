177264c3859d5a4b9864bf806b9dff9fc2562b36
guoyangnice <yangjienice@126.com>
2021-05-27 11:28:09 +0800

    yj@feat:完成Lock-ReentrantLock-多线程打印ABC

---
+++ src/thread/printabcinorder/LockStateDemo.java
@@ -0,0 +1,74 @@
+package thread.printabcinorder;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 可重入锁-ReentrantLock
+ * 这里当初有个疑问，为什么A完成之后不走C呢？因为state的原因，其实A完成之后，A和C可能也抢到了CPU资源，但是因为不符合循环的条件，所以不打印。
+ * @author yangjie
+ */
+public class LockStateDemo {
+    private static Lock lock = new ReentrantLock();
+    private static int state = 0;
+
+    static class ThreadA extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; ) {
+                lock.lock();
+                try {
+                    while (state % 3 == 0) {// 多线程并发，不能用if，必须用循环测试等待条件，避免虚假唤醒
+                        System.out.print("A");
+                        state++;
+                        i++;
+                    }
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+    }
+
+    static class ThreadB extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; ) {
+                lock.lock();
+                try {
+                    while (state % 3 == 1) {// 多线程并发，不能用if，必须用循环测试等待条件，避免虚假唤醒
+                        System.out.print("B");
+                        state++;
+                        i++;
+                    }
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+    }
+
+    static class ThreadC extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; ) {
+                lock.lock();
+                try {
+                    while (state % 3 == 2) {// 多线程并发，不能用if，必须用循环测试等待条件，避免虚假唤醒
+                        System.out.print("C");
+                        state++;
+                        i++;
+                    }
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new ThreadA().start();
+        new ThreadB().start();
+        new ThreadC().start();
+    }
+}
