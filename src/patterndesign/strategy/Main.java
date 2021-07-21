8b8c063d5a6372b877c9740927a33f22e5697013
guoyangnice <yangjienice@126.com>
2021-07-22 00:23:35 +0800

    yj@feat:增加注释

--- src/patterndesign/strategy/Main.java
+++ src/patterndesign/strategy/Main.java
@@ -4,6 +4,7 @@ package patterndesign.strategy;
  * @author yangjie
  * @date 2021/7/22 12:05 上午
  * @desc
+ * 本次的实现是基于之前的项目来的。
  */
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
